2d99c22a0d5ca56ed12ff43c817cfa32d82b9e8f
spark <fabricio.jacob@outlook.com>
2023-05-26 17:46:00 -0300

    add

--- src/test/java/JavaExamplesApplicationTests.java
+++ src/test/java/JavaExamplesApplicationTests.java
@@ -1,12 +1,10 @@
-package com.github.fabriciolfj.javaexamples;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaExamplesApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
